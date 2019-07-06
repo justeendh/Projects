2acd5a95d4016da8c53082fdfa8212fbaf2976f0
Hoàng Hà Duy <hoanghaduy_123@hotmail.com>
2019-07-06 16:05:29 +0700

    update SwaggerResource document

--- irender-backend/services/auth-service/src/main/java/com/service/authservice/AuthServiceApplication.java
+++ irender-backend/services/auth-service/src/main/java/com/service/authservice/AuthServiceApplication.java
@@ -1,15 +1,26 @@
 package com.service.authservice;
 
+import com.google.common.base.Predicates;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
+@EnableSwagger2
 @EnableDiscoveryClient
 @SpringBootApplication
 public class AuthServiceApplication {
@@ -18,10 +29,32 @@ public class AuthServiceApplication {
         SpringApplication.run(AuthServiceApplication.class, args);
     }
 
+    ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("AUTH-API")
+                .description("API for IRENDER AUTH project")
+                .license("IRENDER COMPANY")
+                .licenseUrl("http://irender.vn/")
+                .termsOfServiceUrl("http://irender.vn/")
+                .version("1.0")
+                .contact(new Contact("irender support", "http://irender.vn/", "dev@irender.vn"))
+                .build();
+    }
+
+    @Bean
+    public Docket customImplementation() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))
+                .build()
+                .apiInfo(apiInfo());
+    }
 }
 
 @RestController
-class ServiceInstanceRestController {
+class AuthController {
 
     @Autowired
     private DiscoveryClient discoveryClient;
