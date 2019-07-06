e61c8f96f093906cc0178659602061710871c950
Hoàng Hà Duy <hoanghaduy_123@hotmail.com>
2019-07-06 13:47:44 +0700

    add SwaggerResource

---
+++ irender-backend/cloud/proxy-service/src/main/java/com/cloud/proxy/api/GatewayApi.java
@@ -0,0 +1,54 @@
+package com.cloud.proxy.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableConfigurationProperties({ZuulProperties.class})
+public class GatewayApi {
+
+    private ZuulProperties zuulProperties;
+
+    @Value("${zuul.prefix}")
+    private String zuulPrefix;
+
+    @Autowired
+    public GatewayApi(ZuulProperties zuulProperties) {
+        this.zuulProperties = zuulProperties;
+    }
+
+    @Primary
+    @Bean
+    public SwaggerResourcesProvider swaggerResourcesProvider() {
+        return () -> {
+            List<SwaggerResource> resources = new ArrayList<>();
+            zuulProperties
+                    .getRoutes()
+                    .values()
+                    .forEach(route -> {
+                        if (route.getPath().endsWith("/**") && !route.getId().equals("legacy")) {
+                            resources.add(createResource(route.getServiceId(), route.getId(), route.getPath()));
+                        }
+                    });
+            return resources;
+        };
+    }
+
+    private SwaggerResource createResource(String serviceId, String routeId, String routePath) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(serviceId + ": " + routeId);
+        swaggerResource.setLocation(zuulPrefix + routePath.substring(0, routePath.length() - 3) + "/v2/api-docs");
+        swaggerResource.setSwaggerVersion("2.0");
+        return swaggerResource;
+    }
+}
--- irender-backend/cloud/proxy-service/src/main/java/com/cloud/proxy/proxyservice/ProxyServiceApplication.java
+++ irender-backend/cloud/proxy-service/src/main/java/com/cloud/proxy/proxyservice/ProxyServiceApplication.java
@@ -4,12 +4,16 @@ import com.cloud.proxy.filter.RouteLoggingFilter;
 import com.cloud.proxy.filter.RouterAuthFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableSwagger2
+@EnableHystrix
 public class ProxyServiceApplication {
 
     public static void main(String[] args) {
