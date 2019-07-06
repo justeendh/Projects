e63a1c3d47b5e40f82750a6201917607381f20e8
Hoàng Hà Duy <hoanghaduy_123@hotmail.com>
2019-07-06 16:51:12 +0700

    update SwaggerResource document

--- irender-backend/services/render-service/src/main/java/com/service/renderservice/RenderServiceApplication.java
+++ irender-backend/services/render-service/src/main/java/com/service/renderservice/RenderServiceApplication.java
@@ -1,15 +1,26 @@
 package com.service.renderservice;
 
+import com.google.common.base.Predicates;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
+@EnableSwagger2
 @EnableDiscoveryClient
 @SpringBootApplication
 public class RenderServiceApplication {
@@ -18,6 +29,28 @@ public class RenderServiceApplication {
         SpringApplication.run(RenderServiceApplication.class, args);
     }
 
+    ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("RENDER-API")
+                .description("API for IRENDER RENDER project")
+                .license("IRENDER COMPANY")
+                .licenseUrl("http://irender.vn/")
+                .termsOfServiceUrl("http://irender.vn/")
+                .version("1.0")
+                .contact(new Contact("irender support", "http://irender.vn/", "dev@irender.vn"))
+                .build();
+    }
+
+    @Bean
+    public Docket customImplementation() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))
+                .build()
+                .apiInfo(apiInfo());
+    }
 }
 
 @RestController
